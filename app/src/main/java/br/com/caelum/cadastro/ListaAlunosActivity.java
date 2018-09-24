97f64e36b14eb150737deb76296d31db1c0651d3
Matheus Brandino <m.v.brandino@gmail.com>
2018-09-24 14:33:45 -0300

    criando projeto, usando listview e adapter - ex 2.1

---
+++ app/src/main/java/br/com/caelum/cadastro/ListaAlunosActivity.java
@@ -0,0 +1,27 @@
+package br.com.caelum.cadastro;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class ListaAlunosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_alunos);
+
+        String[] alunos = {"Aldriano", "Andrea",
+                "Heitor", "João", "Gui"};
+
+        ListView lista = findViewById(R.id.lista_alunos);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, alunos);
+
+        lista.setAdapter(adapter);
+
+
+    }
+}
