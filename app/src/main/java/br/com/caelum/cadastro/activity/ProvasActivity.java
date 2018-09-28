ef797eba0bcf1a19b236720cd84f611cc55f9cfa
Matheus Brandino <m.v.brandino@gmail.com>
2018-09-28 12:10:51 -0300

    trabalhando com application resources - 13.8

--- app/src/main/java/br/com/caelum/cadastro/activity/ProvasActivity.java
+++ app/src/main/java/br/com/caelum/cadastro/activity/ProvasActivity.java
@@ -7,6 +7,7 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 
 import br.com.caelum.cadastro.R;
+import br.com.caelum.cadastro.fragments.DetalhesProvaFragment;
 import br.com.caelum.cadastro.fragments.ListaProvasFragment;
 
 public class ProvasActivity extends AppCompatActivity {
@@ -14,16 +15,28 @@ public class ProvasActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_provas);
 
         FragmentManager manager = getSupportFragmentManager();
 
         FragmentTransaction transaction = manager.beginTransaction();
 
-        transaction.replace(R.id.provas_frame, new ListaProvasFragment());
+        if (isLand()) {
+            transaction.replace(R.id.provas_frame_esquerda, new ListaProvasFragment());
+            transaction.replace(R.id.provas_frame_direita, new DetalhesProvaFragment());
+
+        } else {
+
+            transaction.replace(R.id.provas_frame, new ListaProvasFragment());
+        }
 
         transaction.commit();
 
 
+    }
+
+    private boolean isLand() {
+        return getResources().getBoolean(R.bool.land);
     }
 }
